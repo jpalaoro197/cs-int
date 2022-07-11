150120d34a335289f093660e7e6bfcf86b9440db
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-10 21:53:09 -0700

    fix code by adding string so it can be in writeline.

--- DoubleIt.cs
+++ DoubleIt.cs
@@ -4,9 +4,11 @@ class DoubleIt
 {
   static void Main()
   {
-    Console.WriteLine("Give me a number and I will double it for you!");
-    int yourNumber = Console.ReadLine();
+    Console.WriteLine("Give me a number, and I will double it for you!");
+    string stringNumber = Console.ReadLine();  // <-- New code
+    int yourNumber = int.Parse(stringNumber);  // <-- New code
     int yourDoubledNumber = yourNumber * 2;
-    Console.WriteLine("I doubled your number for you: " + yourDoubledNumber);
+    string stringDoubledNumber = yourDoubledNumber.ToString();  // <-- New code
+    Console.WriteLine("I doubled your number for you: " + stringDoubledNumber);
   }
 }
